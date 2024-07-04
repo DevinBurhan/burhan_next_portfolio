ff245b6c6e483b308af2b1d516bf63dfd16956bc
DevinBurhan <burhanuddin.devstree@gmail.com>
2024-07-04 17:00:22 +0530

    second commit

---
+++ src/container/Skills/data.ts
@@ -0,0 +1,106 @@
+export const experiences = [
+  {
+    year: "2024 - Now ",
+    works: [
+      {
+        name: "Software Engineer - Full Stack",
+        company: "Devstree IT Services Private Ltd.",
+        desc: `
+        <div>Implemented <strong>chat with a socket</strong> in the projects, along with <strong>infinite scrolling & Meeting scheduling</strong> features like Calendly. It is in NEXT JS with all advanced features like SSR & SSG.</div>
+        <div><strong>vdocipher integration</strong> in Backend, which does not allow users to take screenshots and video recordings.</div>
+         <div>Developed and maintained backend services for the chatbot.</div>
+         <div>Developed <strong>Pixel Perfect Design</strong> & Serveer side API Calls.</div>
+        `,
+      },
+    ],
+  },
+  {
+    year: "Aprl - 2023",
+    works: [
+      {
+        name: "Frontend Engineer",
+        company: "Devstree IT Services Private Ltd.",
+        desc: `
+        <div>Developed and maintained <strong>RESTFULL APIs</strong> using frameworks like <strong>Express.js and NestJS.</strong></div>
+        <div>Implemented robust <strong>authentication</strong> and <strong>authorization</strong> mechanisms using <strong>JWT and OAuth.</strong></div>
+        <div>Designed and managed <strong>databases</strong> using <strong>MongoDB.</strong></div>
+     
+        `,
+      },
+      // {
+      //   name: "Atatus Product",
+      //   company: "NamLabs Technologies Private Ltd,",
+      //   desc: "Developed full Layout module for Atatus documentation page using Jekyll. Implemented availability synthetic monitor.it includes web monitors, DNS monitoring, SSL monitoring, TCP monitoring, UDP monitoring, CronJob monitor, scheduled task monitoring, heartbeat monitoring and API monitoring.",
+      // },
+      // {
+      //   name: "ReplayBird Product",
+      //   company: "NamLabs Technologies Private Ltd,",
+      //   desc: "Developed Full website(ReplayBird) for end to end individually implemention using Gatsby Framework.",
+      // },
+      // {
+      //   name: "LeadMine Product",
+      //   company: "NamLabs Technologies Private Ltd,",
+      //   desc: "Implemented to store credit/debit card details information powered by stripe and also developed subscription page.",
+      // },
+      // {
+      //   name: "UI Inspector Product",
+      //   company: "NamLabs Technologies Private Ltd,",
+      //   desc: "Worked on most of the feature for this product development both frontend and backend. Developed Dashboard Page, Testcase Page, Plan Page and application page in both front end and back end.(Angular JS and Node JS)",
+      // },
+    ],
+  },
+  {
+    year: "Sept - 2022",
+    works: [
+      {
+        name: "Frontend Engineer",
+        company: "Devstree IT Services Private Ltd.",
+        desc: `
+        <div>Led the successful development of more than <strong>15+ admin panels</strong>, <strong>20+ Landing pages & 5+ e-commerce websites</strong> with consistency & proficiency.</div>
+        <div>Implemented <strong>chat with a socket</strong> in the projects, along with <strong>infinite scrolling & Meeting scheduling</strong> features like Calendly. It is in NEXT JS with all advanced features like SSR & SSG.</div>
+        <div>Live streaming Platform using <strong>vdocipher</strong>, does not allow users to take screenshots and video recordings.</div>
+        <div><strong>3D animations & parallax scrolling</strong> effects using tools like GSAP & FRAMER MOTION.</div>
+      `,
+      },
+    ],
+  },
+  {
+    year: "July - 2022",
+    works: [
+      {
+        name: "Web Developer / Intern",
+        company: "Codding Gujju Private Ltd.",
+        desc: `
+          <div>Have been Worked as web developer and learned <strong>HTML</strong>, <strong>CSS</strong>, <strong>JS</strong>, <strong>Angular</strong>, <strong>Next</strong> and <strong>React</strong>.</div>
+          <div>Developed <strong>Pixel Perfect Design</strong> & Serveer side API Calls.</div>
+        `,
+      },
+    ],
+  },
+] as const;
+
+export const skills = [
+  "html",
+  "css",
+  "javascript",
+  "typescript",
+  "sass",
+
+  "reactjs",
+  "nextjs",
+  "angular",
+  "vue",
+
+  "nodejs",
+  "express",
+  "mongodb",
+  // "mysql",
+  // "redis",
+
+  // "svelte",
+  // "gatsby",
+  // "googlecloud",
+  // "heroku",
+  // "docker",
+  // "python",
+] as const;
